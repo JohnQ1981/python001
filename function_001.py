77c5494936fea4f7eb3827dc4f706315210f07b2
JohnQ1981 <ikram1981@gmail.com>
2022-07-16 22:53:21 -0500

    a

---
+++ function_001.py
@@ -0,0 +1,17 @@
+print("Welcome to Functions")
+for num in range(1,10):
+    print(num)
+count = 0
+for c in range(10):
+    print("Hello Python!")
+    count +=1
+
+print(f"{count}")
+
+for c in range(0,102,2):
+    print(c)
+
+for c in range(102,1,-2):
+    print(c)
+
+    
---
+++ loop_song.py
@@ -0,0 +1,5 @@
+for c in range(100,0,-1):
+    print(f"{c} bottles of beer on the wall")
+    print(f"{c} bottles of beer")
+    print(f"Take one down, pass it around, {c-1} bottles of beer on the wall")
+    print("*"*10)
